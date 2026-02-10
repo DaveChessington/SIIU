9bf13b2997305b14d7e162e7ae95ce63372608ab
David Chessington <davidadavaz@outlook.com>
2026-02-10 17:07:58 -0600

    Customize Django UserAdmin configuration

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+
+    ordering = ('email',)
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff')
+    list_filter = ('role', 'is_active', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Información personal', {'fields': ('first_name', 'last_name', 'building')}),
+        ('Permisos', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'role', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
